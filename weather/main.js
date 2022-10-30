ebd0747ded709e7840cccb808f13a26026f8830d
HackerGprat <41012110+HackerGprat@users.noreply.github.com>
2022-10-30 09:22:13 +0530

    index.html - title fixed

---
+++ weather/main.js
@@ -0,0 +1,4 @@
+const api_key = "586d40c6e788a1ad6e66d2aef729d401";
+const main = document.getElementById('main');
+const form = document.getElementByI('form');
+const search = document.getElementByI('search');
